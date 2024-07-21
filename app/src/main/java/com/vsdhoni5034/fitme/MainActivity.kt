be1ceab87d27885d3620b9a5334bd609d12a329d
Vijay Singh Dhoni <singhvijay2524@gmail.com>
2024-07-21 19:15:20 +0530

    project init

---
+++ app/src/main/java/com/vsdhoni5034/fitme/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.vsdhoni5034.fitme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vsdhoni5034.fitme.ui.theme.FitMeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FitMeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
